3b9b5a44f4bc8b45e23117e7161ef3f986bbbf79
raihan.zafar <rehanraaz7@gmail.com>
2026-03-12 15:44:31 +0530

    Perfect updated Receipt Generator space management

--- src/main/java/com/trust/amanat/controller/RecpdfgenController.java
+++ src/main/java/com/trust/amanat/controller/RecpdfgenController.java
@@ -1,22 +1,8 @@
 package com.trust.amanat.controller;
 
-import com.itextpdf.io.image.ImageData;
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.colors.ColorConstants;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Image;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
-
 import com.trust.amanat.dto.RecPdfGenDTO;
 import com.trust.amanat.entity.RecPdfGenEntity;
 import com.trust.amanat.repository.RecPdfGenRepository;
-
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
