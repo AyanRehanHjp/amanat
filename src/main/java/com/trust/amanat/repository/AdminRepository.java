8b34fbc07f0adca31a0fdb081a631fc346913605
raihan.zafar <rehanraaz7@gmail.com>
2026-05-05 19:22:13 +0530

    Added feature social links more clear

--- src/main/java/com/trust/amanat/repository/AdminRepository.java
+++ src/main/java/com/trust/amanat/repository/AdminRepository.java
@@ -2,6 +2,9 @@ package com.trust.amanat.repository;
 
 import com.trust.amanat.entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
 
