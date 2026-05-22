6ed4d414d916092d1ee0ac7646921f46e7eadc05
raihan.zafar <rehanraaz7@gmail.com>
2026-05-22 16:47:50 +0530

    Icon adjusted insta etc also replaced facebook to whatsapp

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -21,6 +21,16 @@ function instagram(){
 function youtube(){
     window.open(URLS.YOUTUBE, "_blank");
 }
+function whatsappContact(){
+
+window.open(
+"https://wa.me/917277222729?text=" +
+encodeURIComponent("Hi, I want to know more about AWT"),
+"_blank"
+);
+
+}
+
 // 🔹 GLOBAL POPUP
 function showPopup(message){
 
